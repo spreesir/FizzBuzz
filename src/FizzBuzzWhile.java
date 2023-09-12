31904044dd1313caba4d952fdfc2caf4011f3692
spreesir <mo.omar@mail.utoronto.ca>
2023-09-11 20:27:07 -0400

    refactored to extract helper method

--- src/FizzBuzzWhile.java
+++ src/FizzBuzzWhile.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class FizzBuzzWhile {
+public class FizzBuzzWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i < 100) {
+            i = Fizzy(i);
+
+        }
+    }
+
+    private static int Fizzy(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i); }
+        i++;
+        return i;
+    }
 }
