f37064e9fdf9ac8c87f0b8a622c414e94639d82d
spreesir <mo.omar@mail.utoronto.ca>
2023-09-15 12:56:52 -0400

    Changed Fizzy Method from priv to public

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,35 +1,38 @@
 /**
  * Solve the FizzBuzz challenge.
  */
-class FizzBuzz {
-
+public class FizzBuzz {
     public static void main(String[] args) {
+        int i = 1;
+        while(i < 100) {
+            i = Fizzy(i);
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
         }
+    }
+
+    public static int Fizzy(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i); }
+        i++;
+        return i;
     }
 }
 
